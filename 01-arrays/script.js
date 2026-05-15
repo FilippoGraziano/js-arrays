22bc3580fd94a4fcb4e99a7945d717d778fdf62b
Filippo Graziano <filippograziano13@gmail.com>
2026-05-15 15:42:17 +0200

    complete eighth exercise

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -40,4 +40,6 @@ console.log(lewisIndex)
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length;
+if (isTeachersEmpty > 0) console.log(`isTeachersEmpty? No, is ${isTeachersEmpty} words long`);
+else console.log(`isTeacherEmpty? Yes, is empty`)

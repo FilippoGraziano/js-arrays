92efe945b2b6864bb04a839aaf4d18994fa73353
Filippo Graziano <filippograziano13@gmail.com>
2026-05-16 13:53:39 +0200

    fixed first exercise fixed exercise 1 of bonus fixed exercise 2 of bonus fixed exercise 3 of bonus fixed exercise 5 of bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,23 +10,21 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+const reversedTeachers = teachers.toReversed();
 console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
 for (i = 0; i < teachers.length; i++) {
-  let teachersElement;
   if (teachers[i].length >= 5) {
-    teachersElement = teachers[i];
-    longNames.push(teachersElement);
+    longNames.push(teachers[i]);
   }
 };
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(-2, 1);
+teachers.splice(teachers.indexOf(`Ed`), 1);
 console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
@@ -49,5 +47,13 @@ if (isFabioPresent) {
 };
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-let teachersString = teachers.toString();
-console.log(teachersString);
+let row = ``;
+let space = `, `;
+for (i = 0; i < teachers.length; i++) {
+
+  row += teachers[i];
+  if (i !== 5) row += space;
+}
+
+const teachersString = row;
+console.log(teachersString)

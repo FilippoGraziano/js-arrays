2638b2873c5036d23e7be627031e78f357c8996b
Filippo Graziano <filippograziano13@gmail.com>
2026-05-17 21:54:57 +0200

    fixed exercise 1 of bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,7 +10,13 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.toReversed();
+const reverse = [];
+for (i = 0; i < teachers.length; i++) {
+
+  reverse.unshift(teachers[i])
+
+}
+const reversedTeachers = reverse;
 console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
